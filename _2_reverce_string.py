e84a4879d0bb15a7d0ce241d2deb7c2b46fbecf2
TandonPratham <117387692+TandonPratham@users.noreply.github.com>
2022-11-29 09:43:19 +0530

    Update _2_reverce_string.py

--- _2_reverce_string.py
+++ _2_reverce_string.py
@@ -1,9 +1,9 @@
 def string_reverse(str1):
 
-    str1 = ''
+    rstr1 = ''
     index = len(str1)
     while index > 0:
-        str1 += str1[ index - 1 ]
+        rstr1 += str1[ index - 1 ]
         index = index - 1
     return str1
 print(string_reverse('1234abcd'))
